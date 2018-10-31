64b5a3cb6915afbfeab596e80ea165a67a6aacb9
Alexander Denisenko <alexserden@gmail.com>
2018-10-31 19:47:53 +0200

    jdbc

--- src/main/java/repository/JdbcCompaniesRepository.java
+++ src/main/java/repository/JdbcCompaniesRepository.java
@@ -1,5 +1,28 @@
 package repository;
 
-public class JdbcCompaniesRepository {
+import model.Companies;
 
+import java.util.List;
+
+public class JdbcCompaniesRepository implements CompaniesRepository{
+
+    public void save(Companies developers) {
+
+    }
+
+    public void update(Companies developers) {
+
+    }
+
+    public boolean delete(Long id) {
+        return false;
+    }
+
+    public Companies getById(Long id) {
+        return null;
+    }
+
+    public List<Companies> getAll() {
+        return null;
+    }
 }
